2954cc7c64b02520f7a8b37ba73d5d0e24ad0a30
spbryan <seanbryan@comcast.com>
2019-06-10 18:56:42 -0400

    add feature to update inventory quantity through command prompts

--- bamazonManager.js
+++ bamazonManager.js
@@ -55,20 +55,11 @@ function userOptions() {
                 default:
                     closeAndExit();
             }
-            // if (answer.placeOrder) {
-            //     connection.query("SELECT * FROM products", function (err, response) {
-            //         if (err) throw err;
-            //         displayInventory(response);
-            //         purchaseItem();
-            //     })
-            // }
-            // else {
-            //     connection.end();
-            // }
         });
 }
 
 function viewProducts() {
+    clearConsole();
     connection.query("SELECT * FROM products", function (err, response) {
         if (err) throw err;
         displayInventory(response);
@@ -77,13 +68,36 @@ function viewProducts() {
 }
 
 function viewLowInventory() {
-    console.log("Inside viewLowInventory");
-    connection.end();
+    clearConsole();
+    connection.query("SELECT * FROM products WHERE stock_quantity < 5", function (err, response) {
+        if (err) throw err;
+        displayInventory(response);
+        userOptions();
+    })
 }
 
 function addToInventory() {
-    console.log("Inside addToInventory");
-    connection.end();
+    clearConsole();
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "itemID",
+            message: "Enter ID for Product:",
+            validate: function (value) {
+                return validateIsNumeric(value);
+            }
+        },
+        {
+            type: "input",
+            name: "addUnits",
+            message: "Enter Number of Additional Units:",
+            validate: function (value) {
+                return validateIsNumeric(value);
+            }
+        }
+    ]).then(function (answer) {
+        updateQuantity(parseInt(answer.itemID), parseInt(answer.addUnits));
+    });
 }
 
 function addNewProduct() {
@@ -95,7 +109,6 @@ function closeAndExit() {
     console.log("Inside closeAndExit");
     connection.end();
 }
-
 
 /**
  * Select items from the products table
@@ -115,98 +128,48 @@ function displayInventory(response) {
     console.table(tableValues);
 }
 
-/** 
- * Accept console input to purchase a specific number of
- * items from the list
- */
-function purchaseItem() {
-    inquirer
-        .prompt([
-            {
-                name: "itemID",
-                type: "input",
-                message: "Enter Item ID: ",
-                validate: function (value) {
-                    return validateItemID(value);
-                }
-            },
-            {
-                name: "orderQuantity",
-                type: "input",
-                message: "Enter Number of Units: ",
-                validate: function (value) {
-                    return validateQuantity(value);
-                }
-            }
-        ])
-        .then(function (answer) {
-            verifyQuantity(answer.itemID, answer.orderQuantity);
-        });
-}
-
-/**
- * Select item from the data base and verify that enough inventory is
- * available to meet the order
- * @param id 
- * @param quantity 
- */
-function verifyQuantity(id, quantity) {
+function updateQuantity(id, addUnits) {
     connection.query("SELECT * FROM products WHERE item_id=?", [id], function (err, response) {
         if (err) throw err;
-        var stockQuantity = parseInt(response[0].stock_quantity);
-        var orderQuantity = parseInt(quantity);
-        if (stockQuantity >= orderQuantity) {
-            var newQuantity = stockQuantity - orderQuantity;
-            var itemPrice = parseInt(response[0].price);
-            console.log("Total Order Amount: $" + itemPrice * orderQuantity);
-            placeOrder(id, newQuantity);
-        }
-        else {
-            console.log("Insufficient Quantity");
-            purchaseItem();
-        }
+        var name = response[0].product_name;
+        var currentQuantity = parseInt(response[0].stock_quantity);
+        var newQuantity = currentQuantity + addUnits;
+        connection.query("UPDATE products SET ? WHERE ?",
+            [
+                {
+                    stock_quantity: newQuantity
+                },
+                {
+                    item_id: id
+                }
+            ], function (err, response) {
+                if (err) throw err;
+                clearConsole();
+                consoleMessage("Number of " + name + " in stock raised from " +
+                    currentQuantity + " to " + newQuantity);
+                userOptions();
+            })
+
     })
 }
 
-/**
- * Update the data base with the correct values following a successful order
- * @param id 
- * @param newQuantity 
- */
-function placeOrder(id, newQuantity) {
-    connection.query("UPDATE products SET ? WHERE ?",
-        [
-            {
-                stock_quantity: newQuantity
-            },
-            {
-                item_id: id
-            }
-        ], function (err, response) {
-            if (err) throw err;
-            console.log("Order Has Been Placed!");
-            console.log("updated rows: " + response.affectedRows);
-            start();
-        })
+function clearConsole() {
+    process.stdout.write('\x1B[2J\x1B[0f');
+}
+
+function consoleMessage(message) {
+    console.log("  ");
+    console.log("+----------------------------------------------------------+");
+    console.log(message);
+    console.log("+----------------------------------------------------------+");
+    console.log("  ");
 }
 
 /**
- * Validate the input from the ID List
+ * Validate that the requested value is numeric
  * @param value 
  */
-function validateItemID(value) {
-    if (idList.indexOf(value) > -1) {
-        return true;
-    }
-    return "Invalid ID Value";
-}
-
-/**
- * Validate that the requested quatity is numeric 
- * and that there is enough in stock
- * @param value 
- */
-function validateQuantity(value) {
+function validateIsNumeric(value) {
     if (isNaN(value) === true) {
         return "Value Must Be Numeric";
     }
